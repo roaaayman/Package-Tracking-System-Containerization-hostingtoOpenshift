d67e69452788830d340fb7776da40094a18b8c3f
Roaa Ayman <119889441+roaaayman@users.noreply.github.com>
2024-12-21 02:16:57 +0200

    Update registration.go

--- Package-Tracking-System-Containerization-main/backend/registration.go
+++ Package-Tracking-System-Containerization-main/backend/registration.go
@@ -746,7 +746,10 @@ func AssignOrder(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	// MongoDB URI
-	uri := os.Getenv("MONGO_URI")
+	const { MongoClient } = require('mongodb');
+	const uri = "mongodb://database:27017/Package_Tracking_System/";
+
+	const client = new MongoClient(uri);
 
 	clientOptions := options.Client().ApplyURI(uri)
 
